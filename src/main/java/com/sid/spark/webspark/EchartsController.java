42e6017102456c0e1b91542ea722325080c543db
sky李齐 <sky.li@ixiaoshuidi.com>
2019-02-20 09:47:45 +0800

    first commit

---
+++ src/main/java/com/sid/spark/webspark/EchartsController.java
@@ -0,0 +1,37 @@
+package com.sid.spark.webspark;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Sky
+ * create 2019/02/19
+ * email sky.li@ixiaoshuidi.com
+ **/
+@RestController
+public class EchartsController {
+
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    public String sayHello(){
+
+        return "Hello Spring Boot!";
+
+    }
+
+    @RequestMapping(value="/first",method = RequestMethod.GET)
+    public ModelAndView firstDemo(){
+
+        return new ModelAndView("test");//跟templates文件夹下的test.html名字一样，返回这个界面
+
+    }
+
+    @RequestMapping(value="/courseClickCount",method = RequestMethod.GET)
+    public ModelAndView courseClickCountStat(){
+
+        return new ModelAndView("demo");//跟templates文件夹下的demo.html名字一样，返回这个界面
+
+    }
+
+}
