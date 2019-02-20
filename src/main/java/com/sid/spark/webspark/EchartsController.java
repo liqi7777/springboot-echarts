bf0bf842607496ce92679ddb480d662b99ed1d9e
sky李齐 <sky.li@ixiaoshuidi.com>
2019-02-20 14:25:28 +0800

    柱状图

--- src/main/java/com/sid/spark/webspark/EchartsController.java
+++ src/main/java/com/sid/spark/webspark/EchartsController.java
@@ -1,5 +1,6 @@
 package com.sid.spark.webspark;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +14,28 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class EchartsController {
 
-    @RequestMapping(value="/hello",method = RequestMethod.GET)
-    public String sayHello(){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
 
         return "Hello Spring Boot!";
 
     }
 
-    @RequestMapping(value="/first",method = RequestMethod.GET)
-    public ModelAndView firstDemo(){
+    @RequestMapping(value = "/pie", method = RequestMethod.GET)
+    public ModelAndView firstDemo() {
 
-        return new ModelAndView("test");//跟templates文件夹下的test.html名字一样，返回这个界面
+        return new ModelAndView("pie");//跟templates文件夹下的test.html名字一样，返回这个界面
 
     }
 
-    @RequestMapping(value="/courseClickCount",method = RequestMethod.GET)
-    public ModelAndView courseClickCountStat(){
+    @RequestMapping(value = "/bar", method = RequestMethod.GET)
+    public ModelAndView secondDemo(ModelAndView modelAndView) {
+        modelAndView.setViewName("bar");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/courseClickCount", method = RequestMethod.GET)
+    public ModelAndView courseClickCountStat() {
 
         return new ModelAndView("demo");//跟templates文件夹下的demo.html名字一样，返回这个界面
 
